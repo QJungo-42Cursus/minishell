2ce5410104856d13046e1d7fd57e68d9068b022c
Quentin <quentin.jungo@gmail.com>
2022-11-25 00:40:25 +0100

    Normal fully functional. Close to the end of the tokenizer itsefl

--- main.c
+++ main.c
@@ -5,21 +5,60 @@
 #include "tokenizer/tokenizer.h"
 #include "libft/libft.h"
 
-int main() {
+#include "stdlib.h"
+#include "unistd.h"
+#include "fcntl.h"
+#include "sys/wait.h"
 
-	char *line;
-	//line = readline("prompt> ");
 
-	/* TEST TOKENIZER */
-	//line = ft_strdup("echo \" salut\\\" \" "); // OK
-	//line = ft_strdup("echo hello world \" \\\"  salut (avec une chiee d'espace !!!)\"et_un_truc_colle_a_la_fin fin voila quoi... \"\"dquotes_vides");
-	//line = ft_strdup("echo \" salut\\\" \"               \"     "); // ERROR DQUOTE not closed
-	//line = ft_strdup("echo \" salut\\\" \" \"\"     "); // DQUOTE empty and nothiing after
-	line = ft_strdup("echo salut\" s\""); // DQUOTE after word, should concat auto
+void in(t_bool error, char *line)
+{
+	t_list		*tokens;
 
+	if (error)
+	{
+		printf("Error mode -------------\n");
+		if (fork() == 0) // Fork pour pouvoir crash !
+		{
+			printf("line: %s\n len: %d\n\n", line, ft_strlen(line));
+			tokens = tokenizer(line);
+			free(line);
+			printf("\n");
+			return ;
+		}
+		else
+		{
+			wait(NULL);
+			return ;
+		}
+		printf("-------------------- fin error\n\n");
+	}
+	printf("Mode success -------------\n");
 
 	printf("line: %s\n len: %d\n\n", line, ft_strlen(line));
-	t_list *tokens = tokenizer(line);
+	tokens = tokenizer(line); (void)tokens;
 	printf("\n");
-	(void) tokens;
+
+	free(line);
+	printf("-------------------- fin success\n\n");
+}
+
+/* TEST TOKENIZER */
+void test()
+{
+	in(TRUE, ft_strdup("echo \" salut\\\" \"               \"     ")); // ERROR DQUOTE not closed
+	in(FALSE, ft_strdup("echo \" salut\\\" \" ")); // OK
+	in(FALSE, ft_strdup("echo \" salut\\\" \" \"\"     ")); // DQUOTE empty and nothiing after
+	in(FALSE, ft_strdup("echo salut\" s\"")); // DQUOTE after word, should concat auto
+	in(FALSE, ft_strdup("echo ' salut ' ")); // OK
+	in(FALSE, ft_strdup("echo ' salut\\'  '' ")); // QUOTE empty and nothiing after
+	in(FALSE, ft_strdup("echo salut' s'  ")); // QUOTE after word, should concat auto
+	//in(TRUE, ft_strdup("echo ' salut\\'                '     ")); // ERROR QUOTE not closed
+}
+
+int main()
+{
+	//line = readline("prompt> ");
+
+	test();
 }
--- tokenizer/get_next_token_functions.c
+++ tokenizer/get_next_token_functions.c
@@ -2,7 +2,13 @@
 #include "../libft/libft.h"
 #include <stdio.h>
 
+/*
+ * TODO
+ * ajouter un flag pour savoir si on est dans une quote ou une dquote
+ * -> comme ca on peut savoir si l'ats va prendre en compte les $
+ */
 void	get_token_dquote(char *str, int *index, int *start, int *end);
+void	get_token_quote(char *str, int *index, int *start, int *end);
 
 void	log_sep(t_sep sep)
 {
@@ -62,45 +68,35 @@ t_sep	get_sep(char sep)
 /// Gere si ce n'est entouré par rien
 void	get_token_normal(char *str, int *index, int *start, int *end)
 {
-	/// Fake start and end pour pouvoir appeler get_token_(d)quote sans perdre le vrai start
+	/// Fake start pour pouvoir appeler get_token_(d)quote sans perdre le vrai start
 	int		f_start;
-	int		f_end;
 
-	printf("get_token_normal\n");
 	(*start) = *index;
 	while (str[*index] != '\0' && !ft_isspace(str[*index]))
 	{
 		/// Si on tombe sur un backslash, on skip le backslash pour afficher le char suivant
 		if (str[*index] == '\\')
 			ft_strlcpy(str + *index, str + *index + 1, ft_strlen(str + *index));
-		if (str[*index] == '"')
+		if (str[*index] == '"' || str[*index] == '\'')
 		{
-			printf("DQUOTE in normal\n");
-			get_token_dquote(str, index, &f_start, &f_end);
-			*end = f_end;
+			ft_strlcpy(str + *index, str + *index + 1, ft_strlen(str + *index));
+			if (str[*index] == '"')
+				get_token_dquote(str, index, &f_start, end);
+			else
+				get_token_quote(str, index, &f_start, end);
+			return ;
 		}
-		/*
-		else if (str[*index] == '\'')
-		{
-			(*index)++;
-			get_token_quote(str, index, &f_start, &f_end);
-			(*index)++;
-		}
-		*/
-		else
-			(*index)++;
-
 		(*index)++;
 	}
+	(*end) = (*index);
 
-
-
-
+	/*
+	 * Inutile pour le normal ? TODO
 	if (!ft_isspace(str[*index]) && str[*index] != '\0')
 		ft_strlcpy(str + *index, str + *index + 1, ft_strlen(str + *index));
 	while (!ft_isspace(str[*index]) && str[*index] != '\0')
 		(*index)++;
-	(*end) = (*index);
+	*/
 }
 
 /// Gere les double quotes "
@@ -139,24 +135,58 @@ void	get_token_dquote(char *str, int *index, int *start, int *end)
 			ft_strlcpy(str + *index, str + *index + 1, ft_strlen(str + *index));
 		(*index)++;
 	}
+
+	/// S'il reste du texte apres le quote, on le lit jusqu'a un isspace
 	if (!ft_isspace(str[*index]) && str[*index] != '\0')
 		ft_strlcpy(str + *index, str + *index + 1, ft_strlen(str + *index));
 	while (!ft_isspace(str[*index]) && str[*index] != '\0')
 		(*index)++;
+
+	(*end) = (*index);
+}
+
+/// Gere les simples quotes '
+void	get_token_quote(char *str, int *index, int *start, int *end)
+{
+	/// Si c'est le dernier char -> error
+	/// Vu que j'ai trim au debut, pas besoin de chercher apres
+	if (str[*index + 1] == '\0')
+	{
+		// TODO
+		printf("ERROR: QUOTE not closed\n");
+		exit (1);
+		return ;
+	}
+
+	/// Si les quotes sont fermées et vide, on cherche la suite
+	(*index)++;
+	if (str[*index] == '\'')
+	{
+		(*start) = *index + 1;
+		while (!ft_isspace(str[*index]) && str[*index] != '\0')
+			(*index)++;
+		(*end) = *index;
+		printf("QUOTE empty\n");
+		if (str[*index] == '\0')
+			printf("QUOTE empty and end of line\n");
+		return ;
+	}
+
+	/// Sinon mode dquote "normal"
+	(*start) = *index;
+	while (str[*index] != '\0' && str[*index] != '\'')
+		(*index)++;
+
+	/// S'il reste du texte apres le quote, on le lit jusqu'a un isspace
+	if (!ft_isspace(str[*index]) && str[*index] != '\0')
+		ft_strlcpy(str + *index, str + *index + 1, ft_strlen(str + *index));
+	while (!ft_isspace(str[*index]) && str[*index] != '\0')
+		(*index)++;
+
 	(*end) = (*index);
 }
 
 /*
-void	get_token_quote(char *str, int *index, int *start)
-{
-	(*index)++;
-	if (str[*index] == '\'')
-		return ;
-	while (str[*index] != '\0' && str[*index] != '\'')
-		(*index)++;
-	(void)start; // TODO
-}
-
 void	get_token_pipe(char *str, int *index, int *start) // TODO autre term tech ?
 {
 	(void)str;
@@ -188,8 +218,7 @@ t_get_token	*list_func()
 	}
 	list[DQUOTE] = get_token_dquote;
 	list[NONE] = get_token_normal;
-
-	//list[QUOTE] = get_token_quote;
+	list[QUOTE] = get_token_quote;
 	//list[PIPE] = get_token_pipe;
 	//list[AMPERSAND] = get_token_ampersant;
 	//list[SEMICOLUMN] = get_token_semicolumn;
