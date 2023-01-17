6d2d8298b3e2e6caf59f7dc56785bfbac2695ac8
Quentin <quentin.jungo@gmail.com>
2023-01-17 19:36:32 +0100

    echo.c termine

--- builtins/echo.c
+++ builtins/echo.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
 #include "../libft/libft.h"
+#include <string.h>
 
-// TODO est-ce que le argv[0] doit contenir le nom echo dans ce cas ??
-// Est-ce qu'on gere la transformation en varriable ici ? (je dirait non)
-int	echo(char **argv /*, char **envp*/)
+int	echo(char **argv)
 {
 	t_bool	n_mode;
 	int		i;
 
-	if (argv == NULL)
-	{
-		// TODO
-		return (ERROR);
-	}
-	if (*argv == NULL)
-	{
-		// TODO
-		return (ERROR);
-	}
-	if (ft_strlen(*argv) == 2 && ft_strncmp(*argv, "-n", 2) == 0)
-	{
-		n_mode = TRUE;
-		argv++;
-	}
-	else
-	{
-		n_mode = FALSE;
-	}
-	
-
-	i = 0;
+	i = 1;
+	n_mode = (ft_strncmp(argv[i], "-n", 3) == 0);
+	if (n_mode)
+		i++;
 	while (argv[i] != NULL)
 	{
 		printf("%s", argv[i]);
+		if (argv[i + 1] != NULL)
+			printf(" ");
 		i++;
 	}
 	if (n_mode == FALSE)
 		printf("\n");
 	return (SUCCESS);
 }
+
+/*
+int main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)envp;
+	echo(argv);
+	return (0);
+}
+*/
