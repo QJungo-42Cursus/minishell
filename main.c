7f876361a881c1a1b2b3f2427d47eeab8410b4b5
Alexis Gonelle <alexisgonelle.mobile@gmail.com>
2023-01-19 21:20:25 +0100

    travaille sur les main

--- main.c
+++ main.c
@@ -14,47 +14,21 @@
 #include "tests/debug_helper.h"
 #include "minishell.h"
 
-/*
-int	check_input(char *input)
+static int	check_input(char *input)
 {
 	if (input == NULL)
-		return (1);
+		return ('0');
 	else if (ft_strlen(input) == 0)
-		return (1);
+		return ('0');
 	else if (ft_strncmp(input, "exit", 5) == 0)
 	{
 		rl_clear_history();
-		return (0);
+		return ('e');
 	}
 	else
 	{
 		add_history(input);
 		return (1);
-	}
-}
-*/
-
-int main_loop()
-{
-	char	*cmd_input;
-	char	*prompt_msg;
-
-	prompt_msg = ft_strdup("minishell $>");
-	while (1)
-	{
-		cmd_input = readline(prompt_msg);
-		/*
-		if (check_input(cmd_input))
-		{
-			printf("'%s' can't be handle, since we are not doing much (for now).\n", cmd_input);
-			free(cmd_input);
-		}
-		*/
-		{
-			free(cmd_input);
-			free(prompt_msg);
-			return (0);
-		}
 	}
 }
 
@@ -68,29 +42,68 @@ static int	init_minishell(t_minishell *minishell, char **envp)
 	minishell->env_paths = get_paths_in_env(envp);
 	if (minishell->env_paths == NULL)
 	{
-		split_free(minishell->env_paths);// TODO rename
+		split_free(minishell->env_paths);
 		return (ERROR);
 	}
 	return (SUCCESS);
 }
 
+static int	main_minishell(t_minishell *minishell, char *valid_input)
+{
+	t_list	*tokens;
+
+	(void) minishell;
+	tokens = tokenizer(valid_input);
+	if (tokens == NULL)
+		return (ERROR);
+	return (SUCCESS);
+}
+
+static int	main_loop(t_minishell *minishell)
+{
+	char	*cmd_input;
+	int		cmd_code;
+	char	*prompt_msg;
+
+	prompt_msg = ft_strdup("minishell $>");
+	while (1)
+	{
+		cmd_input = readline(prompt_msg);
+		cmd_code = check_input(cmd_input);
+		if (cmd_code != 'e')
+		{
+			if (cmd_code != '0')
+				if (main_minishell(minishell, cmd_input) == ERROR)
+
+			free(cmd_input);
+		}
+		else
+		{
+			free(cmd_input);
+			free(prompt_msg);
+			return (0);
+		}
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	t_minishell minishell;
-	(void) argc; (void) argv;
-	// TODO balek des args ?
-	
+	t_minishell	minishell;
+
+	(void) argc;
+	(void) argv;
 	if (init_minishell(&minishell, envp) == ERROR)
 		return (EXIT_FAILURE);
-	//main_loop();
-	
+	main_loop(&minishell);
+	/*
 	//char **test = ft_split("export CACA=0", ' ');
-	char **cd_test = ft_split("cd /", ' ');
+	//char **cd_test = ft_split("cd /", ' ');
 	//char **untest = ft_split("unset LS_COLORS", ' ');
 	//export_(&minishell, test);
 	//unset(&minishell, untest);
 	env(minishell);
 	cd(&minishell, cd_test);
 	env(minishell);
-
+	*/
+	return (0);
 }
