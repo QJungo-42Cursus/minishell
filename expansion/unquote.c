8922815ed101ad40c17b7668e8be75e274bba492
Quentin <quentin.jungo@gmail.com>
2023-03-07 11:20:35 +0100

    tokenizer and unquote working finally fine

--- expansion/unquote.c
+++ expansion/unquote.c
@@ -1,86 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unquote.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qjungo <qjungo@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 11:18:38 by qjungo            #+#    #+#             */
+/*   Updated: 2023/03/07 11:19:43 by qjungo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft/libft.h"
-#include <stdio.h>
+#include "../tokenizer/tokenizer.h"
 
-void	iunquote(char *str)
-{
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	printf("before: |%s|\n", str);
-	while (str[i] != '\0')
-	{
-		printf("%d -> %c \t\t", i, str[i]);
-		if (is_in_charset(str[i], "\'\""))
-			i++;
-		if (str[i] == '\0')
-			break ;
-		str[j] = str[i];
-		printf("%d -> %c \n", i, str[i]);
-		i++;
-		j++;
-	}
-	str[j] = '\0';
-	printf("unquoted: |%s|\n\n", str);
-}
-
-
-char	set_sep(char c)
-{
-	if (is_in_charset(c, "\'\""))
-	{
-		printf("ptn de sep |%c|\n", c);
-		return (c);
-	}
-	return (0);
-}
-
-t_bool	move_cursor(int *i, int *j, char *str, char *sep)
-{
-	while (str[*i] != '\0' && !is_in_charset(str[*i], "\'\""))
-	{
-		(*i)++;
-		(*j)++;
-	}
-	*sep = set_sep(str[*i]);
-	if (ft_strlen(str) == *i)
-		return ((t_bool)TRUE);
-	(*i)++;
-	return ((t_bool)FALSE);
-}
-
+// 1. trouver le debut (" ou ')
+// 2. trouver la fin
+// 3. memmove
+// 4. recursivite
 void	unquote(char *str)
 {
-	int		i;
-	int		j;
-	char	sep;
+	int			i;
+	t_position	pos;
+	char		sep;
 
 	i = 0;
-	j = 0;
-	if (move_cursor(&i, &j, str, &sep))
-		return ;
-	if (str[i] == sep)
-	{
-		printf("consssss\n");
-		ft_memmove(&str[i - 1], &str[i + 1], ft_strlen(&str[i + 1]) + 1);
-		unquote(&str[i - 1]);
-		return ;
-
-	}
-
-	while (str[i] != '\0')
-	{
-		if (str[i] == sep)
-		{
-			printf("in: %s\n\n", &str[i]);
-			unquote(&str[i]);
-			return ;
-		}
-		str[j] = str[i];
+	while (str[i] != '\0' && !is_in_charset(str[i], "\'\""))
 		i++;
-		j++;
-	}
-	str[j] = '\0';
-	printf("unquoted: |%s|\n\n", str);
+	if (str[i] == '\0')
+		return ;
+	sep = str[i];
+	pos.start = i;
+	i++;
+	while (str[i] != '\0' && str[i] != sep)
+		i++;
+	if (str[i] == '\0')
+		return ;
+	ft_memmove(&str[pos.start], &str[pos.start + 1],
+		ft_strlen(&str[pos.start + 1]) + 1);
+	pos.end = i - 1;
+	ft_memmove(&str[pos.end], &str[pos.end + 1],
+		ft_strlen(&str[pos.end + 1]) + 1);
+	unquote(str);
 }
